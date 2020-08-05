7e2faf3a81d81b1e6fdca874ebe35d44a1d28dae
Rory Folster <rory-folster@live.nmit.ac.nz>
2020-08-06 10:27:57 +1200

    Activity 3 Completed (I Think)

---
+++ src/Activity 3/3.js
@@ -0,0 +1,19 @@
+// function setup() {
+//     createCanvas(100, 100, WEBGL);
+//   }
+  
+//   function draw() {
+//     background(205, 105, 94);
+//     rotateX(frameCount * 0.01);
+//     rotateZ(frameCount * 0.01);
+//     cylinder(40, 70);
+//   }
+
+function setup() {
+    createCanvas(100, 100, WEBGL);
+  }
+  
+  function draw() {
+    background(400, 200, 150);
+    ellipsoid(40, 50, 40);
+  }
